41bab450d427850cc9447215a2101a093c309ace
Ranbeer Singh <Ranbeersinghcauhan@gmail.com>
2022-08-04 09:31:03 +0530

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by RANBEER SINGH on 04/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
